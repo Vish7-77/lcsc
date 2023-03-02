92261e23373393e9626c25c881cdaa87cbf6a00e
vishal <Vishalgehlot334@gmail.com>
2023-03-02 16:02:10 +0530

    ok

--- script.js
+++ script.js
@@ -1,6 +1,112 @@
-const  submitTheForm=()=>{
-
-    alert('form submitted successfully')
-}
 
 
+
+
+// let x =["studentone","studenttwo","studentthreee"]
+
+
+
+// x.map((ele)=>console.log(ele))
+        
+
+// x.push(7,8,9,10)
+
+// console.log(x.indexOf("studenttwo"))
+
+
+
+// console.log(x.includes('studenttwo'))
+
+
+
+
+// let a = {
+// Firstinstall:2000,
+// Secondinstall:5000,
+
+
+// }
+
+
+// console.log(a.Firstinstall+a.Secondinstall)
+
+
+
+
+// functions
+
+//normal
+//arrow function
+
+
+
+// function add(){
+
+// }
+
+//defining a function
+// const addition =()=>{
+
+// }
+
+
+
+//declearing a function
+// addition()
+
+
+
+
+
+// const add =(a,b)=>{
+
+// let c = a+b
+// console.log(c)
+
+
+// }
+
+
+
+// add(2,3)
+
+
+
+// const GST=(a)=>{
+
+//    let x = typeof(a)
+
+// if(x=="number"){
+//    let c = a/100*18;
+//    let total =Number(a)+Number(c)
+//    console.log(c +" this is the gst price for" + a +"and the total is"+ total )
+
+// }
+// else{
+//    console.log('the input should be in number format')
+// }
+
+
+// }
+
+
+
+
+
+
+
+//operators + -/ % *
+// == (checking) ,&&  || !=
+
+
+
+
+// GST(56)
+
+
+
+// const printName=(name)=>{
+//    console.log("hello"+" " +name + " this is your full stack class")
+// }
+// const x= ['student001','student002','student003','student004','student005','student006']
+// x.map((ele)=>printName(ele))
