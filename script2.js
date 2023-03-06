3c0e2be869693ddea6730f279d862900e1188438
vishal <Vishalgehlot334@gmail.com>
2023-03-06 16:51:21 +0530

    ok

--- script2.js
+++ script2.js
@@ -1,24 +1,27 @@
-let total= []
+// let total= []
+
+// //function with argu, param
+// const Add = (e)=>{
 
 
-const Add = (e)=>{
 
 
-let x = e
+// //maths. Opertaions    
+// let x = e
   
-total.push(x)
+// total.push(x)
 
-
-
-let t =total.reduce((a,b)=>a+b)
-// console.log(t)
-
-document.getElementById('products').innerText=t
+// //
 
 
 
 
 
+// let t =total.reduce((a,b)=>a+b)
+// // console.log(t)
+
+// //DOM
+// document.getElementById('products').innerText=t
 
 
 
@@ -26,11 +29,112 @@ document.getElementById('products').innerText=t
 
 
 
-}
 
 
 
-const Clr=()=>{
-    total=[]
-    document.getElementById('products').innerText=0
-}
+
+
+// }
+
+
+// //without any argu. params.
+// const Clr=()=>{
+//     total=[]
+//     document.getElementById('products').innerText=0
+// }
+
+
+
+
+
+// const FunctionOne =()=>{
+
+// console.log('hello')
+
+// }
+
+
+
+// const FunctionTwo =(e)=>{
+
+// console.log(e)
+    
+// }
+
+// FunctionOne();
+// FunctionTwo("world")
+// FunctionTwo("hello")
+// FunctionTwo("this")
+
+
+// let x=100
+
+// const hello =()=>
+// {
+//     console.log(x)
+// }
+
+
+// hello()
+
+
+
+// let a =100
+
+
+
+
+// const hello=()=>
+// {
+//     let b=100;
+//     console.log(b)
+// }
+
+
+
+// console.log(a)
+// console.log(b)
+
+
+// const a = "this is full stack class"    
+
+
+// console.error(a)
+// console.log(a)
+// console.warn(a)
+
+
+// // console.log(a.split(' '))
+// let x =a.split(' ')
+
+// // console.log(x[0]);
+// // console.log(x[1]);
+// // console.log(x[2]);
+// // console.log(x[3]);
+// // console.log(x[4]);
+
+// x.map((ele)=>console.log(ele))
+
+
+// let a  =[1,2,4,5,5,5,6,5,6,45,6,34,5,34,5,23,4,23,4,32,4,23,4,23,34,123,4,3 ]
+
+//this is the string here
+let  b = "this is full stack class"
+
+
+
+//converted to an array
+let c = b.split(' ')
+
+
+
+//mapping through it : consoling the elements
+c.map((ele,index)=>console.log(index,ele))
+
+
+
+
+
+
+// a.map((ele)=>console.log(ele))
+// console.log(a)
