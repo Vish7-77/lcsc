1b96c909707d4a353cd723480828b101fb13558b
vishal <Vishalgehlot334@gmail.com>
2023-03-08 16:48:25 +0530

    DOM

--- script.js
+++ script.js
@@ -1,27 +1,80 @@
-// const printTheName=(stdname)=>{
-// console.log(`our institution have student name ${stdname}, and done very well in exams`)
-// }
-// let Arr =['studentOne','studentTwo','studentThree','StudentFour','studentOne','studentTwo','studentThree','StudentFour']
-// Arr.map((ele)=>printTheName(ele))
 
-
-//we are creating a new array <== elements
-//we are using a map function of array . 
-//map function will loop through the array and execute the PrintNAme Function on Every element
-//Print NAme  function takes the argument and console it in the templete literal form
+//target the btn
+// const btn = document.getElementById('btn')
 
 
 
 
-// let a ="Print NAme function takes the argument and console it in the templete literal form"
-// let b = a.split(' ')
-// console.log(b)
-// b.map((ele)=>console.log(ele))
+
+// // this is the event listener
+// btn.addEventListener('click',()=>{
+//    //creating a new Para
+// const NewPara = document.createElement('p')
+// NewPara.setAttribute('class','styling')
+// NewPara.innerText="this is New para"
+// //targetting the body element
+// const NewBody= document.body
+// NewBody.append(NewPara)
+
+
+
+// })
+
+
+// const h1 = document.querySelector('h1')
+
+
+// h1.addEventListener('mouseenter',()=>{
+
+
+
+// h1.innerText="you are in h1 "
+
+// const div= document.querySelector('div')
+// div.innerText="you are in of h1 this is in the div "
+// div.style.color='white'
+// div.style.background='red'
+// const btn = document.querySelector('button')
+// btn.style.filter='blur(10px)'
+// btn.style.transform="translateX(0px)"
+
+
+
+// })
+
+
+
+// h1.addEventListener('mouseleave',()=>{
+
+
+
+//     const btn = document.querySelector('button')
+// btn.style.filter='blur(0px)'
+// btn.style.transform="translateX(50px)"
+// h1.innerText="you are out of h1"
+// const div= document.querySelector('div')
+// div.innerText="you are out of h1 this is in the div "
+// div.style.color='red'
+// div.style.background='white'
+
+// })
+
+
+
+// const h1 = document.querySelector('h1')
+
+// addEventListener('click',()=>{console.log(clientX)})
 
 
 
 
-// let Arr =[1,2,3,4,5,6,7,8,9,10]
-// let total =Arr.reduce((a,b)=>a+b)
+// console.log(window.document.body)
 
-// console.log(total)
+
+
+// console.log()
+// console.warn()
+// console.error()
+
+
+
